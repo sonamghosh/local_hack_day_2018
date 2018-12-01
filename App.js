65d34fdda6af560cc51d8e7d1169271d138e1a5e
U-WIT\luih <luih@WIT45000398.wit.private>
2018-12-01 14:26:32 -0500

    Added camera capability, snapping picture and pulling the uri of the photo

--- App.js
+++ App.js
@@ -1,21 +1,107 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar
+} from "react-native";
+import { Camera, Permissions } from "expo";
 
-export default class App extends React.Component {
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+
+  snap = async () => {
+    if (this.camera) {
+      console.log("SNAPPING PICTURE");
+      let photo = await this.camera.takePictureAsync();
+      console.log(photo.uri);
+    }
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar hidden />
+          <Camera
+            style={styles.camera}
+            type={this.state.type}
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
+            <TouchableOpacity
+              style={styles.flipContainer}
+              onPress={() => {
+                this.setState({
+                  type:
+                    this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                });
+              }}
+            >
+              <Text style={styles.flip}> FLIP </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.snap} style={styles.snapContainer}>
+              <Text style={styles.snap}>SNAP PICTURE</Text>
+            </TouchableOpacity>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  camera: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "center"
+  },
+  snapContainer: {
+    flex: 1,
+    opacity: 0.8,
+    height: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 1
+  },
+  snap: {
+    color: "#87ceeb",
+    margin: 5
+  },
+  flipContainer: {
+    flex: 1,
+    opacity: 0.8,
+    height: "10%",
+    color: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#87ceeb",
+    borderRadius: 1
+  },
+  flip: {
+    color: "#fff",
+    margin: 5
+  }
 });
