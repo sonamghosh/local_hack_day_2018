ba6be92c3cf5cad34ecd34c9b9c842e06a84bbf3
Sonam Ghosh <sonamghosh@gmail.com>
2018-12-01 12:18:49 -0500

    Put functionality into a function

--- upload_to_imgur.js
+++ upload_to_imgur.js
@@ -1,10 +1,18 @@
-var imgur = require('imgur');
+// This Script uploads images from local machine to imgur and provide the link
 
-// Single Image
-imgur.uploadFile('./dank_pics_01.jpg')
+function uploadImgur(filename) {
+  var imgur = require('imgur');
+  // Upload Single File
+  imgur.uploadFile(filename)
+
   .then(function (json) {
+    var link = json.data.link;
     console.log(json.data.link);
+    return link
   })
   .catch(function (err) {
     console.err(err.message);
   });
+}
+
+uploadImgur('./dank_pics_02.jpg')
