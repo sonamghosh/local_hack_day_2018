931246c7f29dbbd81aeabcc538565d5179f3f8db
U-WIT\luih <luih@WIT45000398.wit.private>
2018-12-01 17:28:08 -0500

    Add all files. Server is working

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const multer = require("multer");
+const upload = multer({
+  dest: "uploads/" // this saves your file into a directory called "uploads"
+});
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// It's very crucial that the file name matches the name attribute in your html
+app.post("/", upload.single("file-to-upload"), (req, res) => {
+  res.statusCode = 200;
+  res.end("Received file: " + req);
+});
+
+app.listen(port, () => {
+  console.log("Server running at http://localhost:" + port);
+});
