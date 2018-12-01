fa177c1b53a2c8a3d2c39dcdf6b3bb0e912692b5
Sonam Ghosh <sonamghosh@gmail.com>
2018-12-01 10:57:31 -0500

    Testbed for Azure using node

---
+++ analyze-image.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const request = require('request');
+
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = 'ea8b096680c24990a45eba7976aa9c15';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase =
+    'https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze';
+
+const imageUrl =
+    'http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+// Request parameters.
+const params = {
+    'visualFeatures': 'Categories,Description,Color',
+    'details': '',
+    'language': 'en'
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
